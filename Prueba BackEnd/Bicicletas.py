b67c658b7f648d3305ec5278c7945d7dd986cb3f
Natalia Paredes <natalia.paz.paredes@hotmail.com>
2023-10-01 18:38:26 -0300

    arregle lo de la tv

--- Prueba BackEnd/Bicicletas.py
+++ Prueba BackEnd/Bicicletas.py
@@ -1,9 +1,9 @@
 from Transporte import Transporte
 
 class Bicicletas(Transporte):
-    def __init__(self, aro, peso, precio, marca):
+    def __init__(self, aro, peso, precio, marca, despacho):
+        super().__init__(peso, despacho)
         self.__aro = aro
-        self.__peso = peso
         self.__precio = precio
         self.__marca = marca
 
@@ -19,19 +19,13 @@ class Bicicletas(Transporte):
     def set_aro(self, aro):
         self.__aro = aro
 
-    def get_peso(self):
-        return self.__peso
-    
-    def set_peso(self, peso):
-        self.__peso = peso
-
     def get_marca(self):
         return self.__marca
     
     def set_marca(self, marca):
         self.__marca
 
-    def calcular_costo_despacho(self, precio):
+    def calcular_costo_despacho(self):
         return 4000 + self.__peso * self.__precio
     
     def cotizar(self):
@@ -39,4 +33,4 @@ class Bicicletas(Transporte):
         return precio
 
     def __str__(self):
-        return f'Aro: {self.__aro} \n Peso: {self.__peso} \n Precio: {self.__precio} \n Marca: {self.__marca}'
+        return f'{super().__str__()} \n Precio {self.__precio}\n Aro: {self.__aro} \n Marca {self.__marca}'
--- Prueba BackEnd/Tvs.py
+++ Prueba BackEnd/Tvs.py
@@ -13,8 +13,9 @@ class Tvs(Tecnologia):
 
     def cotizar(self):
         descuento_eficiencia = self.calcular_descuento()
-        precio_con_descuento = self.__precio * (1 - descuento_eficiencia)
-        return f"Marca: {self.__marca}\nVoltaje: {self.__voltaje}\nEficiencia: {self.__eficiencia}\nPrecio: ${self.__precio}\nTamaño: {self.__tamaño} pulgadas\nDescuento aplicado: {descuento_eficiencia * 100}%\nPrecio total: ${precio_con_descuento:}"
+        precio_con_descuento = self.get_precio() * (1 - descuento_eficiencia)
+        return f"Marca: {self.get_precio()}\nVoltaje: {self.get_voltaje()}\nEficiencia: {self.get_eficiencia()}\nPrecio: ${self.get_precio()}\nTamaño: {self.get_tamaño()} pulgadas\nDescuento aplicado: {descuento_eficiencia * 100}%\nPrecio total: ${precio_con_descuento:}"
     
     def __str__(self):
         return f'{super().__str__()} \n Tamaño {self.__tamaño}'
+    
